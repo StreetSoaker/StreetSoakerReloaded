d39805041f991858a424684567d07d2f91030dd0
Koen van den Heuvel <koenvandeheuvel@gmail.com>
2013-11-25 12:43:15 +0100

    Added Files
    Added socket.IO
    Updated Jquery
    Added map to view

---
+++ http-server/app/js/streetsoaker.app.js
@@ -0,0 +1,17 @@
+function initialize() {
+	google.maps.visualRefresh = true;
+
+	var mapOptions = {
+		center: new google.maps.LatLng(-34.397, 150.644),
+		zoom: 8
+	};
+	var map = new google.maps.Map(document.getElementById("mapView"),
+		mapOptions);
+	}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function getLocation(method) {
+
+}
+
