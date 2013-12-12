3eeea143dfbdb9aa6fa8ed9c674063491c3e5814
Koen van den Heuvel <koenvandeheuvel@gmail.com>
2013-12-12 14:59:33 +0100

    Completed gamelist.js

--- AndroidApp/StreetSoaker/www/js/streetsoaker.gamelist.js
+++ AndroidApp/StreetSoaker/www/js/streetsoaker.gamelist.js
@@ -1,30 +1,42 @@
-socket.on('connect', function() {
-	socket.emit('getGames', 'name', function(data){
-		console.log(data);
-		console.log('test');
-	});
+/**
+* Generates the gamelist with data from the server
+*
+* @author	Koen van den Heuvel
+*/
 
+socket.on('connect', function() {
+	//Datasource containing active games
 	var gameList = new kendo.data.DataSource({
 		transport: {
 			read: function(options){
+				//Request games object from nodejs server
 				socket.emit('getGames', '', function(data){
-					console.log(data);
-					console.log('test');
+
+					//Convert the main object to a array containing game objects
 					games = $.map(data, function(value, index) {
 						return [value];
 					});
+
+					//Pass the games array to the datasource
 					options.success(games);
 				});
 			}
 		},
-		group: "private",
+		//Group games by private and public games
+		group:  {field: "private", dir: "desc"},
+
+		//Sort all games based on name on ascending order
+		sort: {field: "name", dir: "asc" }
 	});
 
+	//Generate Listview on gameListView ID
 	$("#gameListView").kendoMobileListView({
 		dataSource: gameList,
 		pullToRefresh: true,
-		filterable: false,
 		style: "inset",
-		template: '<a data-icon="globe"><span class="gameTitle">#: name #</span><span class="gameMode">#: gameMode #</span><span class="gamePlayers">#: playerAmount #/#: maxPlayers #</span></a>',
+		template: '#if(private == "Public"){# <a data-icon="globe"> #}else{#  <a data-icon="ssLock"> #}# <span class="gameTitle">#: name #</span><span class="gameMode">#: gameMode #</span><span class="gamePlayers">#: playerAmount #/#: maxPlayers #</span></a>',
+		click: function(e) {
+			joinGame(e.dataItem.id);
+		}
 	});
 });
